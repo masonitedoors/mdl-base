ad37f90830234d20ec956f6410eec48dfbcea797
Joseph Fusco <jfusco@masonite.com>
2020-08-22 08:13:40 -0400

    Remove duplicate source code

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   stories: ['../docs/**/*.stories.(js|mdx)'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-a11y',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-actions',
-  ],
+  addons: ['@storybook/addon-essentials'],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,37 @@
-import { addParameters } from '@storybook/html';
-
 // Import mdl-core.scss into all stories.
 import '!style-loader!css-loader!sass-loader!./../src/scss/mdl-core.scss';
 import '!style-loader!css-loader!sass-loader!./preview.scss';
 
-// @storybook/addon-backgrounds
-addParameters({
-  backgrounds: [
-    {
-      name: 'gray',
-      value: '#F3F3F3',
-      default: true,
-    },
-    {
-      name: 'white',
-      value: '#FFFFFF',
-    },
-  ],
-});
+export const parameters = {
+  viewMode: 'docs',
+  layout: 'padded',
+  docs: {
+    transformSource: (src) => sanitizeSource(src),
+  },
+  backgrounds: {
+    default: 'gray',
+    values: [
+      {
+        name: 'gray',
+        value: '#F3F3F3',
+      },
+      {
+        name: 'white',
+        value: '#FFFFFF',
+      },
+    ],
+  },
+};
+
+function sanitizeSource(src) {
+  // Remove backticks.
+  src = src.replace(/`/gm, '');
+
+  // Remove groups of 4 spaces.
+  src = src.replace(/^(    )/gm, '');
+
+  // Remove groups of 2 spaces.
+  src = src.replace(/^(  )/gm, '');
+
+  return src;
+}
